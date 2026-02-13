dff0c1b111ab0ad88dd28eac6d0c072dd2b44140
Niklas Vorberg <services@vorberg-webmail.de>
2026-02-13 15:31:46 +0100

    Initial Commit

---
+++ Cloudwrkz/CloudwrkzApp.swift
@@ -0,0 +1,32 @@
+//
+//  CloudwrkzApp.swift
+//  Cloudwrkz
+//
+//  Created by Niklas Vorberg on 13.02.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CloudwrkzApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
